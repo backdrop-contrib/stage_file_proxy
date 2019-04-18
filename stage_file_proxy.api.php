0bdddea9a05695b5e947839d4534b4c341b99463
Gregory Netsas <klonos@gmail.com>
2019-04-18 13:06:35 +1000

    Remove parameter from hook_stage_file_proxy_excluded_paths_alter

--- stage_file_proxy.api.php
+++ stage_file_proxy.api.php
@@ -10,10 +10,8 @@
  *
  * @param array $excluded_paths
  *   An array (passed by reference) of the list of paths.
- * @param string $uri
- *   The uri of the file.
  */
-function hook_stage_file_proxy_excluded_paths_alter(array &$excluded_paths, $uri) {
+function hook_stage_file_proxy_excluded_paths_alter(array &$excluded_paths) {
   // If this is a advagg path, ignore it.
   if (module_exists('advagg')) {
     $excluded_paths[] = '/advagg_';
