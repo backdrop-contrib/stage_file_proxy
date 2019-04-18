ac8387bf2a717c039e01e3c90f02d0f07acdd7c0
Gregory Netsas <klonos@gmail.com>
2019-04-18 11:11:24 +1000

    Add API documentation.

---
+++ stage_file_proxy.api.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the stage_file_proxy module.
+ */
+
+/**
+ * Alter the list of paths that should be excluded by stage file proxy.
+ *
+ * @param array $engines
+ *   An array (passed by reference) of the list of paths.
+ * @param string $uri
+ *   The uri of the file.
+ */
+function hook_stage_file_proxy_excluded_paths_alter(array &$excluded_paths, $uri) {
+  // If this is a advagg path, ignore it.
+  if (module_exists('advagg')) {
+    $excluded_paths[] = '/advagg_';
+  }
+}
